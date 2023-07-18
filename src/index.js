e449387866f3a9fbe1b1d8de8624effa259d8111
gatotbima1104 <gatotbima2002@gmail.com>
2023-07-18 18:16:02 +0700

    fix inisializate ga

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ReactGA from 'react-ga';
+
+// initialize Google Analytics
+ReactGA.initialize('G-2R9R6C3KGR');
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
